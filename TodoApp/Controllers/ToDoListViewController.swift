357996a147a04424ab9826faf05ae8edf9b164d8
GeraGarza <josegerag@hotmail.com>
2018-06-17 21:06:38 -0500

    Persisting data using Codable protocol and Plist

--- TodoApp/Controllers/ToDoListViewController.swift
+++ TodoApp/Controllers/ToDoListViewController.swift
@@ -9,21 +9,29 @@
 import UIKit
 
 class ToDoListViewController: UITableViewController {
-
     
-    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
+    
+   // let defaults = UserDefaults.standard
     //store keyvalue pairs through app
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String]{
-            itemArray = items
-        }
+    
+        print(dataFilePath)
         
-        // Do any additional setup after loading the view, typically from a nib.
+
+        loadItems()
+
+//        if let items = defaults.array(forKey: "TodoListArray") as? [Item]{
+//            itemArray = items
+//        }
+        
+       //  Do any additional setup after loading the view, typically from a nib.
     }
 
     
@@ -35,8 +43,12 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell" , for: indexPath)
       
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
+        //ternary
+        cell.accessoryType = item.done ? .checkmark : .none
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        
         return cell
     
         
@@ -51,13 +63,13 @@ class ToDoListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
        // print (itemArray[indexPath.row])
         //grabbing reference to the cell at a perticular path
-
-        //desolves the click/grey color after click
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
-        tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        }else{
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+       
+      
+        
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+        saveItems()
+        
         tableView.deselectRow(at: indexPath, animated: true)
   
         
@@ -74,10 +86,11 @@ class ToDoListViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once user clicks add item button on alert
-
-            self.itemArray.append(textField.text!)
-            self.defaults.set(self.itemArray, forKey: "TodoListArray")
-            self.tableView.reloadData()
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+            
+            self.saveItems()
             
         }
         
@@ -95,7 +108,33 @@ class ToDoListViewController: UITableViewController {
    
     }
     
+    func saveItems(){
+        let encoder = PropertyListEncoder()
+        do{
+            let data = try encoder.encode(self.itemArray)
+            try data.write(to: self.dataFilePath!)
+        }catch{
+            print("Error encoding item array , \(error)")
+        }
+        
+        self.tableView.reloadData()
+        
+        
+    }
     
+    func loadItems(){
+        if let data = try? Data(contentsOf: dataFilePath!){
+            let decoder = PropertyListDecoder()
+            
+            do{
+            itemArray = try decoder.decode([Item].self, from: data)
+            }catch{
+                print("Error decoding item arrat, \(error)")
+            }
+        
+        }
+        
+    }
     
 }
 
---
+++ TodoApp/Data Model/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TodoApp
+//
+//  Created by Gera Garza on 6/15/18.
+//  Copyright © 2018 Gera Garza. All rights reserved.
+//
+
+import Foundation
+
+class Item: Codable{
+    
+
+var title : String = ""
+var done : Bool = false
+
+
+}
